892a166616551ac7090eb9d5c59ed07600aede1f
kmc97 <kmc97@duke.edu>
2018-04-13 12:58:51 -0400

    encodes and decodes jpg into base64

---
+++ image_processing/manipulate_image.py
@@ -0,0 +1,14 @@
+import base64
+
+def image_to_string(filename):
+    with open(filename, "rb") as image_file:
+        image_string = base64.b64encode(image_file.read())
+        return image_string
+
+def string_to_image(base64string, new_file_name):
+    with open(new_file_name, "wb") as image_out:
+        image_out.write(base64.b64decode(base64string))
+
+image_string = image_to_string("go_bobcats.jpg")
+print(image_string)
+string_to_image(image_string, "bobkittens.jpg")
