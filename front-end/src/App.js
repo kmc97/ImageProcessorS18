6869dc4b80c9d4a0a545e3cd150bde069b130883
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-13 10:12:04 -0400

    added title

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-
+            <Title />
       </div>
     );
   }
