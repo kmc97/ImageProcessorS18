1776fade9a0116468ee93c377130d7064adbe64c
kmc97 <kmc97@duke.edu>
2018-04-18 12:28:09 -0400

    base64 photo read in

--- image_processing/manipulate_image.py
+++ image_processing/manipulate_image.py
@@ -1,4 +1,5 @@
 import base64
+from skimage import data, io, filters
 
 def image_to_string(filename):
     with open(filename, "rb") as image_file:
@@ -6,7 +7,10 @@ def image_to_string(filename):
         return image_string
 
 def string_to_image(base64string, new_file_name):
+# just change the extensions for picture
     with open(new_file_name, "wb") as image_out:
         image_out.write(base64.b64decode(base64string))
 
 
+string = image_to_string('go_bobcats.jpg')
+string_to_image(string,'bobkittens.tiff')
