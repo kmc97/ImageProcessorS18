0da724464450b4c81f3ddefbf797d6d6aa3a8648
deeptig94 <dg194@duke.edu>
2018-04-22 14:22:45 -0400

    posts tuple of filename and base64 to mongo

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pymodm import connect
-from pymodm import MongoModel, fields
+from pymodm import MongoModel, fields, connect
 from PIL import Image
 import numpy as np
 from gridfs import GridFS
 import base64
 
-#connect("mongodb://")
 app = Flask(__name__)
+CORS(app)
+connect("mongodb://localhost:27017/imageprocessor")
 
 
-@app.route('/image_processor', methods=['POST'])
-def image_to_string(request, filename):
-    with open(request.GET[filename], "rb") as image_file:
-        image_string = base64.b64encode(image_file.read())
-    print(image_string)
-    abc = db.database_name.insert({"image": image_string})
-    return abc
+@app.route('/imageprocessor', methods=['POST'])
+def original_image():
+    r = request.get_json()
+    name = r["file_name"]
+    base64_image = r["base_64"]
 
+    x = jsonify({'file_name': name, 'base_64': base64_image})
 
-@app.route('/image_processor', methods=['GET'])
-def string_to_image(base64string, new_file_name):
-    with open(new_file_name, "wb") as image_out:
-        image_out.write(base64.b64decode(base64string))
+    return x
 
 
 #def open_image(image_filename):
@@ -42,4 +40,7 @@ def string_to_image(base64string, new_file_name):
 #@app.route('/imageprocessor/<image_filename>', methods=['GET'])
 #def disp_image(image_filename):
 
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1")
 
