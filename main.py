ae6f8121826683f426e0e17af42c52800e18c5de
deeptig94 <dg194@duke.edu>
2018-04-22 15:33:05 -0400

    putting data into mongo

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymodm import connect
 from pymodm import MongoModel, fields, connect
-from PIL import Image
+import models
 import numpy as np
-from gridfs import GridFS
 import base64
 
 app = Flask(__name__)
@@ -12,7 +10,7 @@ CORS(app)
 connect("mongodb://localhost:27017/imageprocessor")
 
 
-@app.route('/imageprocessor', methods=['POST'])
+@app.route('/imageprocessor/original_image', methods=['POST'])
 def original_image():
     r = request.get_json()
     name = r["file_name"]
@@ -21,6 +19,31 @@ def original_image():
     x = jsonify({'file_name': name, 'base_64': base64_image})
 
     return x
+
+
+@app.route('/imageprocessor/original_image/<file_name>', methods=['GET'])
+def get_data(file_name):
+
+    user = models.User.objects.raw({'_id': file_name}).first()
+    image = user.base_64
+    print(image)
+
+
+@app.route('/imageprocessor/processed_image', methods=['POST'])
+def processed_image():
+    r = request.get_json()
+    name_p = r["file_name_p"]
+    base64_image_p = r["base_64_p"]
+
+    y = jsonify({'file_name_p': name_p, 'base_64_p': base64_image_p})
+
+    return y
+
+
+def add_original_image(name, base64_image):
+
+    u = models.User(name, base64_image)
+    u.save()
 
 
 #def open_image(image_filename):
