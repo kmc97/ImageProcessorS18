074ab884d6b3d6c76ef3faf90527e984f1443327
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-22 14:40:42 -0400

    submit button

---
+++ react-learning/src/Submit.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+
+export default class Submit extends React.Component {
+ /*
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+*/
+ render () {
+   return (
+     <Button variant="raised">
+        Submit Image
+     </Button>
+   )
+ }
+}
+
