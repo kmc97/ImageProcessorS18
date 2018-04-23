41905b913e1ccd230201d31631633216ee0da6d5
kmc97 <kmc97@duke.edu>
2018-04-22 23:01:55 -0400

    add reverse video and doc strings

--- image_processing/back_end.py
+++ image_processing/back_end.py
@@ -3,36 +3,78 @@ import numpy as np
 from scipy import misc
 from PIL import Image
 import datetime
+import logging
 
-from manipulate_image import string_to_image, pic_to_numpy, numpy_to_pic, image_to_string
-from image_process import convert_gray, contrast_stretching, pic_metrics, time_now, process_time, adaptive_equalization, histogram_equalization
+from manipulate_image import string_to_image, pic_to_numpy, numpy_to_pic, image_to_string, check_b64
+from image_process import convert_gray, contrast_stretching, pic_metrics, time_now, process_time, adaptive_equalization, histogram_equalization, invert
 from Histogram import pic_histo, display_histogram
 
 def process_contrast_stretch(UUID, b64_string):
+    """ Function takes in Unique ID and b64 image string, contrast stretches the image and outputs the Unique ID, time it took to process the image, image metrics and the b64 of the edited image
+
+    :param UUID: User input, unique identifier where file will be pulled
+    :param b64_string:  Recieved from front-end, contains b64 of desired image
+    :raises TypeError: if input data is not base64
+    :returns b64_processed: A list containing: the unique identifier, duration of processing, image metrics and the b64 string of the processed image with the histogram
+    """
+    try:
+        check_b64(b64_string)
+    except:
+        raise TypeError('data type is not base64')
+        
     t1 = time_now()
-    img = string_to_image(b64_string, 'image.jpg')
-    img_np = convert_gray('image.jpg')
+    img = string_to_image(b64_string, UUID + '.jpg')
+    img_np = convert_gray(UUID + '.jpg')
     contrast = contrast_stretching(img_np)
     metrics = pic_metrics(contrast)
-    display_histogram(contrast, 'image.jpg')
-    b64_p = image_to_string('image.jpg')
+    display_histogram(contrast, UUID + '.jpg')
+    b64_p = image_to_string(UUID + '.jpg')
     dur_process = process_time(t1)
     b64_processed =  [UUID, dur_process, metrics, b64_p]
     return b64_processed
 
 def process_adapt_equalization(UUID, b64_string):
+    
+    """ Function takes in Unique ID and b64 image string, adaptively equalizes the image and outputs the Unique ID, time it took to process the image, image metrics and the b64 of the edited image with the histogram
+
+    :param UUID: User input, unique identifier where file will be pulled
+    :param b64_string:  Recieved from front-end, contains b64 of desired image
+    :raises TypeError: if input data is not base64
+    :returns b64_processed: A list containing: the unique identifier, duration of processing, image metrics and the b64 string of the processed image with the histogram
+    """
+ 
+    try:
+        check_b64(b64_string)
+    except:
+        raise TypeError('data type is not base64')
+        
     t1 = time_now()
-    img = string_to_image(b64_string, 'image.jpg')
-    img_np = convert_gray('image.jpg')
+    img = string_to_image(b64_string, UUID + '.jpg')
+    img_np = convert_gray(UUID + '.jpg')
     adapt_eq = adaptive_equalization(img_np)
     metrics = pic_metrics(adapt_eq)
-    display_histogram(adapt_eq, 'image.jpg')
-    b64_p = image_to_string('image.jpg')
+    display_histogram(adapt_eq, UUID + '.jpg')
+    b64_p = image_to_string(UUID + '.jpg')
     dur_process = process_time(t1)
     b64_processed =  [UUID, dur_process, metrics, b64_p]
     return b64_processed
 
 def process_histogram_equalization(UUID, b64_string):
+
+    """ Function takes in Unique ID and b64 image string, histogram equalizes the image and outputs the Unique ID, time it took to process the image, image metrics and the b64 of the edited image
+
+    :param UUID: User input, unique identifier where file will be pulled
+    :param b64_string:  Recieved from front-end, contains b64 of desired image
+    :raises TypeError: raises error if input is not base64
+    :returns b64_processed: A list containing: the unique identifier, duration of processing, image metrics and the b64 string of the processed image with the histogram
+
+    """
+    
+    try:
+        check_b64(b64_string)
+    except:
+        raise TypeError('data type is not base64')
+        
     t1 = time_now()
     img = string_to_image(b64_string, UUID + '.jpg')
     img_np = convert_gray(UUID + '.jpg')
@@ -44,3 +86,30 @@ def process_histogram_equalization(UUID, b64_string):
     b64_processed =  [UUID, dur_process, metrics, b64_p]
     return b64_processed
 
+
+
+def process_reverse_image(UUID, b64_string):
+    
+    """ Function takes in Unique ID and b64 image string, and inverts the image and outputs the Unique ID, time it took to process the image, image metrics and the b64 of the edited image with the histogram
+
+    :param UUID: User input, unique identifier where file will be pulled
+    :param b64_string:  Recieved from front-end, contains b64 of desired image
+    :raises TypeError: if input data is not base64
+    :returns b64_processed: A list containing: the unique identifier, duration of processing, image metrics and the b64 string of the processed image with the histogram
+    """
+ 
+    try:
+        check_b64(b64_string)
+    except:
+        raise TypeError('data type is not base64')
+        
+    t1 = time_now()
+    img = string_to_image(b64_string, UUID + '.jpg')
+    img_np = convert_gray(UUID + '.jpg')
+    rev_image = invert(img_np)
+    metrics = pic_metrics(rev_image)
+    display_histogram(rev_image, UUID + '.jpg')
+    b64_p = image_to_string(UUID + '.jpg')
+    dur_process = process_time(t1)
+    b64_processed =  [UUID, dur_process, metrics, b64_p]
+    return b64_processed
--- image_processing/manipulate_image.py
+++ image_processing/manipulate_image.py
@@ -2,10 +2,23 @@ import numpy as np
 import base64
 from scipy import misc
 from PIL import Image
+import logging
 
 def image_to_string(filename):
+
+    """ Function transforms image to b64 to numpy array
+   
+    :param filename: image that will be transformed to b64
+    :returns image_np: numpy array containing base64
+    :raises ValueError: value error if image_string is empty
+
+    """
+
     with open(filename, "rb") as image_file:
         image_string = base64.b64encode(image_file.read())
+        if image_string is None:
+            raise ValueError("picture not converted correctly")
+            logging.warning("b64 string has nothing in it")        
         image_np = np.array(image_string)
         return image_np
 
@@ -23,5 +36,5 @@ def numpy_to_pic(np_array, filename):
     img = Image.fromarray(np_array)
     img.save(filename)
 
-#pic = pic_to_numpy('UCL.jpg')
-#numpy_to_pic(pic,'test.jpg')
+def check_b64(b64):
+    base64.b64encode(base64.b64decode(b64)) == b64
