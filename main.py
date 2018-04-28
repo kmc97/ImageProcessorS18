c5942390801240e14a092aad770d6056a6e0ba6e
deeptig94 <dg194@duke.edu>
2018-04-28 15:57:52 -0400

    added all processing functions

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from flask_cors import CORS
 from pymodm import MongoModel, fields, connect
 import models
 from models import return_entry, add_file
-import logging
 
-from image_processing.back_end import process_contrast_stretch, process_adapt_equalization, \
+from image_processing.back_end import process_contrast_stretch, process_adapt_equalization,\
     process_histogram_equalization, process_reverse_image, process_log_compression
 
-
-logging.basicConfig(filename='logging.txt', format='%(asctime)s %(message)s', datefmt ='%m/%d/%Y &I:%M:%S %p',
-                    level=logging.DEBUG)
+import numpy as np
+import base64
 
 app = Flask(__name__)
 CORS(app)
@@ -41,6 +39,55 @@ def original_image():
         info[5] = info[5].decode("utf-8")
 
         add_file(info[0], info[1], info[2], info[3], metrics_output, info[5])
+
+    if image_proc_type == "adaptive equalization":
+        info = process_adapt_equalization(name, b64_string, export_file_type)
+        metrics_list = list(info[4])
+        num_pixels = metrics_list[0]
+        pic_size = metrics_list[1]
+        avg_value = metrics_list[3]
+        metrics_output = [num_pixels, pic_size, avg_value]
+        info[5] = info[5].decode("utf-8")
+
+        add_file(info[0], info[1], info[2], info[3], metrics_output, info[5])
+
+    if image_proc_type == "histogram equalization":
+        info = process_histogram_equalization(name, b64_string, export_file_type)
+        metrics_list = list(info[4])
+        num_pixels = metrics_list[0]
+        pic_size = metrics_list[1]
+        avg_value = metrics_list[3]
+        metrics_output = [num_pixels, pic_size, avg_value]
+        info[5] = info[5].decode("utf-8")
+
+        add_file(info[0], info[1], info[2], info[3], metrics_output, info[5])
+
+    if image_proc_type == "reverse video":
+        info = process_reverse_image(name, b64_string, export_file_type)
+        metrics_list = list(info[4])
+        num_pixels = metrics_list[0]
+        pic_size = metrics_list[1]
+        avg_value = metrics_list[3]
+        metrics_output = [num_pixels, pic_size, avg_value]
+        info[5] = info[5].decode("utf-8")
+
+        add_file(info[0], info[1], info[2], info[3], metrics_output, info[5])
+
+    if image_proc_type == "log compression":
+        info = process_log_compression(name, b64_string, export_file_type)
+        metrics_list = list(info[4])
+        num_pixels = metrics_list[0]
+        pic_size = metrics_list[1]
+        avg_value = metrics_list[3]
+        metrics_output = [num_pixels, pic_size, avg_value]
+        info[5] = info[5].decode("utf-8")
+
+        add_file(info[0], info[1], info[2], info[3], metrics_output, info[5])
+
+    print_this = {"filename": info[5]}
+    return jsonify(print_this)
+
+
 
 
 @app.route('/imageprocessor/original_image/getthedata/<filename>', methods=['GET'])
