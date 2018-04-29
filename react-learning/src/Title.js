f545dd1a800b0dc4869f51d3303a76cce8e067a4
Souhaila Noor <souhaila@Souhailas-MacBook-Air.local>
2018-04-29 11:41:10 -0400

    added file after it was deleted

---
+++ react-learning/src/Title.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+var styles = {
+  "appBarStyle": {
+    "marginBottom": "50px",
+    "marginLeft":"10px",
+    "textAlign":"center",
+    "backgroundColor": "green",
+  }
+}
+
+export default class Title extends React.Component {
+  render() {
+    return (
+      <div>
+      <AppBar position="static" style={styles.appBarStyle}>
+            <Toolbar>
+                <Typography variant="display3" gutterBottom align="right">
+                    Image Processing
+                </Typography>
+            </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
